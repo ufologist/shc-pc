e5fac75a5f05265bb7df577837fb02b1890f3f13
ufologist <klsforever@gmail.com>
2019-06-29 17:31:38 +0800

    修改接口地址

--- test/index.js
+++ test/index.js
@@ -3,22 +3,22 @@ import HttpClient from '../src/shc-pc.js';
 var httpClient = new HttpClient();
 
 httpClient.send({ // 正常
-    url: 'http://localhost:8005/admin/order-view/purchase-promote'
+    url: 'http://localhost:8005/hello'
 }).then(function([data]) {
     console.log('data', data);
 });
 
 httpClient.send({ // A
-    url: 'http://localhost:8006/admin/order-view/operate-log'
+    url: 'http://localhost:8006/can-not-get'
 });
 httpClient.send({ // H
     url: 'https://httpbin.org/status/404'
 });
 httpClient.send({ // B
-    url: 'http://localhost:8005/admin/order-view/operate-log'
+    url: 'http://localhost:8005/status1'
 });
 httpClient.send({ // C
-    url: 'http://localhost:8005/admin/order-view/operate-log',
+    url: 'http://localhost:8005/status2',
     transformResponse: [function(data) {
         console.log(a.b);
         return data;
